4b4ced913007b083c9adb640c5dbae85b7174770
MINN <110530872+IrinSong@users.noreply.github.com>
2023-01-20 17:14:37 +0900

    add weather

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+const API_KEY = "42424fb15926e3ee27885778a40a8bf5"
+
+function onGeoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  fetch(url)
+  .then((response) => response.json())
+  .then(data => {
+    const weather = document.querySelector("#weather span:first-child");
+    const temp = document.querySelector("#weather span:nth-child(2)");
+    const city = document.querySelector("#weather span:nth-child(3)");
+    weather.innerText = data.weather[0].main;
+    temp.innerText = data.main.temp;
+    city.innerText = data.name;
+  });
+}
+
+function onGeoError() {
+  alert("불러올 수 있는 날씨가 없습니다.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
